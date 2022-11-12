7c62a73f6c347a9cfd797a7fa5a834f20a187982
jeanc <jeancamlap@gmail.com>
2022-11-12 11:46:02 +0100

    test selenium

--- utils/driverUtils.py
+++ utils/driverUtils.py
@@ -14,18 +14,9 @@ def get_driver():
     # create a new instance of the Chrome driver
     # set download directory
     chromeOptions = Options()
-    chromeOptions.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2})
     chromeOptions.add_argument("--no-sandbox")
-    chromeOptions.add_argument("--disable-setuid-sandbox")
-
-    chromeOptions.add_argument("--remote-debugging-port=9222")  # this
-
-    chromeOptions.add_argument("--disable-dev-shm-using")
-    chromeOptions.add_argument("--disable-extensions")
-    chromeOptions.add_argument("--disable-gpu")
-    chromeOptions.add_argument("start-maximized")
-    chromeOptions.add_argument("disable-infobars")
     chromeOptions.add_argument("--headless")
+    chromeOptions.add_argument("--disable-gpu")
     prefs = {'download.default_directory': os.getcwd()}
     chromeOptions.add_experimental_option('prefs', prefs)
     return webdriver.Chrome(options=chromeOptions)
