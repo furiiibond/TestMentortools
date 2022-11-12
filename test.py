3b5af6b3e5ab22559484283a16d890547bb86a3b
jeanc <jeancamlap@gmail.com>
2022-11-12 10:25:42 +0100

    test selenium

--- test.py
+++ test.py
@@ -1,16 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-CHROME_PATH = '/usr/bin/google-chrome'
-CHROMEDRIVER_PATH = '/usr/bin/chromedriver'
 WINDOW_SIZE = "1920,1080"
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
-chrome_options.binary_location = CHROME_PATH
 
-driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,
+driver = webdriver.Chrome(
                           chrome_options=chrome_options
                           )
 driver.get("https://www.google.com")
