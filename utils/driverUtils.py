238b37b422aa493c96969610a56dae3f9339c545
jeanc <jeancamlap@gmail.com>
2022-11-12 11:22:46 +0100

    test selenium

--- utils/driverUtils.py
+++ utils/driverUtils.py
@@ -14,6 +14,8 @@ def get_driver():
     # create a new instance of the Chrome driver
     # set download directory
     chrome_options = Options()
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
